8892626a1cb63867a55d34dd4591a8f439e406fc
synapse0426 <synapselee@gmail.com>
2019-09-25 10:56:18 +0900

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+// 1)프로젝트의 상태에 변화를 일으키는 것을 액션이라고 한다.
+//   가장 먼저 액션에 이름을 정의 해줘야 한다.
